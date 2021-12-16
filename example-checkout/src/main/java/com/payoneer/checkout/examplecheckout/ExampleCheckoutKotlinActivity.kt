b1092e5646b8030d81b60153fbc0c0c8ecc0b9d3
Ferdinand Bada <33724380+Fbada006@users.noreply.github.com>
2021-12-16 19:20:46 +0300

    PCX-2484: add kotlin activity to checkout module

---
+++ example-checkout/src/main/java/com/payoneer/checkout/examplecheckout/ExampleCheckoutKotlinActivity.kt
@@ -0,0 +1,22 @@
+/*
+ *
+ *  Copyright (c) 2021 Payoneer Germany GmbH
+ *  https://www.payoneer.com
+ *
+ *  This file is open source and available under the MIT license.
+ *  See the LICENSE file for more information.
+ *
+ */
+
+package com.payoneer.checkout.examplecheckout
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ExampleCheckoutKotlinActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_examplecheckout)
+    }
+}

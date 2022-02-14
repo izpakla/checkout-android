5bdf1829ce602c1bf396b2725fc98266dfcf61e9
Ferdinand Bada <33724380+Fbada006@users.noreply.github.com>
2022-02-14 16:52:30 +0300

    PCX-2738: set up kotlin

--- example-shop/src/main/java/com/payoneer/checkout/exampleshop/shared/ShopException.kt
+++ example-shop/src/main/java/com/payoneer/checkout/exampleshop/shared/ShopException.kt
@@ -5,21 +5,9 @@
  * This file is open source and available under the MIT license.
  * See the LICENSE file for more information.
  */
-
-package com.payoneer.checkout.exampleshop.shared;
+package com.payoneer.checkout.exampleshop.shared
 
 /**
  * Generic ShopException
  */
-public class ShopException extends Exception {
-
-    /**
-     * {@inheritDoc}
-     *
-     * @param detailMessage the detail message
-     * @param cause the cause
-     */
-    public ShopException(final String detailMessage, final Throwable cause) {
-        super(detailMessage, cause);
-    }
-}
+class ShopException(detailMessage: String?, cause: Throwable?) : Exception(detailMessage, cause)

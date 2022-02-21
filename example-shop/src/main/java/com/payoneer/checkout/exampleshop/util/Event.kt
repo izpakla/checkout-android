11728f3c23d0a8702b7e7a1c4de825a5481bff9d
Ferdinand Bada <33724380+Fbada006@users.noreply.github.com>
2022-02-21 12:33:05 +0300

    PCX-2738: move event utils to package

--- example-shop/src/main/java/com/payoneer/checkout/exampleshop/util/Event.kt
+++ example-shop/src/main/java/com/payoneer/checkout/exampleshop/util/Event.kt
@@ -1,14 +1,14 @@
 /*
  *
- * Copyright (c) 2022 Payoneer Germany GmbH
- * https://www.payoneer.com
- *
- * This file is open source and available under the MIT license.
- *See the LICENSE file for more information.
- *
+ *  * Copyright (c) 2022 Payoneer Germany GmbH
+ *  * https://www.payoneer.com
+ *  *
+ *  * This file is open source and available under the MIT license.
+ *  * See the LICENSE file for more information.
+ *  *
  */
 
-package com.payoneer.checkout.exampleshop
+package com.payoneer.checkout.exampleshop.util
 
 import androidx.lifecycle.MutableLiveData
 

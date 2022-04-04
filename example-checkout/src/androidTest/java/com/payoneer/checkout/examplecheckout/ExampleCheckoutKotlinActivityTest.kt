39a308646022a10713630c7ab95e5d534f8dfc93
Fbada006 <ferdinandkamata@yahoo.com>
2022-04-04 18:01:21 +0300

    PCX-3040: add tests for both java and kotlin checkout apps

--- example-checkout/src/androidTest/java/com/payoneer/checkout/examplecheckout/ExampleCheckoutKotlinActivityTest.kt
+++ example-checkout/src/androidTest/java/com/payoneer/checkout/examplecheckout/ExampleCheckoutKotlinActivityTest.kt
@@ -4,32 +4,53 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
-import org.junit.Assert.*
+import androidx.test.rule.ActivityTestRule
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
-class ExampleCheckoutKotlinActivityTest : BaseKotlinTest() {
+class ExampleCheckoutKotlinActivityTest {
+
+    @Suppress("DEPRECATION")
+    @get:Rule
+    val rule = ActivityTestRule(ExampleCheckoutJavaActivity::class.java)
 
     @Test
-    fun clickingButtonWithEmptyUrlConfirmErrorDialogShown() {
-        onView(ViewMatchers.withId(R.id.button_show_payment_list)).perform(click())
+    fun clickingListButtonWithEmptyUrlConfirmErrorDialogShown() {
+        onView(withId(R.id.button_show_payment_list)).perform(click())
 
-        onView(withText("Please paste a valid List Url in the input field."))
+        onView(withText("Error"))
             .check(matches(isDisplayed()))
     }
 
     @Test
-    fun clickingButtonWithInvalidUrlConfirmErrorDialogShown() {
-        onView(ViewMatchers.withId(R.id.input_listurl)).perform(typeText("somelistUrl"))
+    fun clickingListButtonWithInvalidUrlConfirmErrorDialogShown() {
+        onView(withId(R.id.input_listurl)).perform(typeText("somelistUrl"))
 
-        onView(ViewMatchers.withId(R.id.button_show_payment_list)).perform(click())
+        onView(withId(R.id.button_show_payment_list)).perform(click())
+
+        onView(withText("Error"))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun clickingPresetButtonWithEmptyUrlConfirmErrorDialogShown() {
+        onView(withId(R.id.button_charge_preset_acount)).perform(click())
+
+        onView(withText("Error"))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun clickingPresetButtonWithInvalidUrlConfirmErrorDialogShown() {
+        onView(withId(R.id.input_listurl)).perform(typeText("somelistUrl"))
+
+        onView(withId(R.id.button_charge_preset_acount)).perform(click())
 
         onView(withText("Please paste a valid List Url in the input field."))
             .check(matches(isDisplayed()))

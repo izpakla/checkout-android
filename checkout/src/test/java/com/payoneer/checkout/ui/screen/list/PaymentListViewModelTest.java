a25c3c77e5f1cc0f53f3f7514857af21f54e6f2d
Fbada006 <ferdinandkamata@yahoo.com>
2022-06-08 11:49:44 +0300

    PCX-3458: add tests for processing preset card without error

--- checkout/src/test/java/com/payoneer/checkout/ui/screen/list/PaymentListViewModelTest.java
+++ checkout/src/test/java/com/payoneer/checkout/ui/screen/list/PaymentListViewModelTest.java
@@ -10,17 +10,35 @@
 
 package com.payoneer.checkout.ui.screen.list;
 
+import static com.payoneer.checkout.redirect.RedirectService.INTERACTION_CODE;
+import static com.payoneer.checkout.redirect.RedirectService.INTERACTION_REASON;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 import androidx.test.core.app.ApplicationProvider;
 
 import com.payoneer.checkout.CheckoutConfiguration;
+import com.payoneer.checkout.CheckoutResult;
 import com.payoneer.checkout.account.DeleteAccountInteractor;
+import com.payoneer.checkout.model.Interaction;
+import com.payoneer.checkout.model.Parameter;
+import com.payoneer.checkout.model.PresetAccount;
+import com.payoneer.checkout.model.Redirect;
 import com.payoneer.checkout.payment.PaymentServiceInteractor;
+import com.payoneer.checkout.ui.model.PaymentSession;
+import com.payoneer.checkout.ui.model.PresetCard;
 import com.payoneer.checkout.ui.session.PaymentSessionInteractor;
+import com.payoneer.checkout.util.ContentEvent;
+import com.payoneer.checkout.util.LiveDataUtil;
+import com.payoneer.checkout.util.Resource;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 @RunWith(RobolectricTestRunner.class)
 public class PaymentListViewModelTest {
@@ -33,6 +51,44 @@ public class PaymentListViewModelTest {
 
     private final PaymentListViewModel viewModel =
             new PaymentListViewModel(ApplicationProvider.getApplicationContext(), sessionInteractor, serviceInteractor, accountInteractor);
+
+    @Test
+    public void loadPaymentSession_setsLoadingLiveDataCorrectly() throws InterruptedException {
+        viewModel.loadPaymentSession();
+
+        Resource<PaymentSession> showPaymentSession = LiveDataUtil.getOrAwaitValue(viewModel.showPaymentSession());
+
+        assertNotNull(showPaymentSession);
+        assertEquals(showPaymentSession.getStatus(), Resource.LOADING);
+    }
+
+    @Test
+    public void processPaymentCard_shouldSetCorrectDataForPresetCard() throws InterruptedException {
+        List<Parameter> parameters = new ArrayList<>();
+        Parameter codeParam = new Parameter();
+        codeParam.setName(INTERACTION_CODE);
+        codeParam.setValue("code");
+        Parameter reasonParam = new Parameter();
+        reasonParam.setName(INTERACTION_REASON);
+        reasonParam.setValue("reason");
+        parameters.add(codeParam);
+        parameters.add(reasonParam);
+
+        Redirect redirect = new Redirect();
+        redirect.setParameters(parameters);
+        PresetAccount account = new PresetAccount();
+        account.setRedirect(redirect);
+
+        PresetCard card = new PresetCard(account, "", null);
+
+        viewModel.processPaymentCard(card, null);
+
+        ContentEvent<CheckoutResult> resultContentEvent = LiveDataUtil.getOrAwaitValue(viewModel.closeWithCheckoutResult());
+        Interaction interaction = resultContentEvent.getContentIfNotHandled().getInteraction();
+        assertNotNull(interaction);
+        assertEquals(interaction.getCode(), "code");
+        assertEquals(interaction.getReason(), "reason");
+    }
 
     private URL createUrl() {
         try {
---
+++ checkout/src/test/java/com/payoneer/checkout/util/LiveDataUtil.java
@@ -0,0 +1,44 @@
+/*
+ *
+ *   Copyright (c) 2022 Payoneer Germany GmbH
+ *   https://www.payoneer.com
+ *
+ *   This file is open source and available under the MIT license.
+ *   See the LICENSE file for more information.
+ *
+ */
+
+package com.payoneer.checkout.util;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+public class LiveDataUtil {
+
+    @SuppressWarnings("unchecked")
+    public static <T> T getOrAwaitValue(LiveData<T> liveData) throws InterruptedException {
+        final T[] data = (T[]) new Object[]{null};
+        CountDownLatch latch = new CountDownLatch(1);
+
+        Observer<T> observer1 = new Observer<T>() {
+            @Override
+            public void onChanged(T t) {
+                data[0] = t;
+                latch.countDown();
+                liveData.removeObserver(this);
+            }
+        };
+
+        liveData.observeForever(observer1);
+
+        // Don't wait indefinitely if the LiveData is not set.
+        if (!latch.await(2, TimeUnit.SECONDS)) {
+            throw new InterruptedException("LiveData value was never set.");
+        }
+
+        return data[0];
+    }
+}

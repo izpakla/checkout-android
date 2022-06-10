eb41b95e7185897c546e7e041bfd20049ceaffc2
Fbada006 <ferdinandkamata@yahoo.com>
2022-06-10 15:00:06 +0300

    PCX-3458: add tests in ProcessPaymentViewModelTest

--- checkout/src/test/java/com/payoneer/checkout/ui/screen/payment/ProcessPaymentViewModelTest.java
+++ checkout/src/test/java/com/payoneer/checkout/ui/screen/payment/ProcessPaymentViewModelTest.java
@@ -10,8 +10,49 @@
 
 package com.payoneer.checkout.ui.screen.payment;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
+import androidx.test.core.app.ApplicationProvider;
+
+import com.payoneer.checkout.CheckoutConfiguration;
+import com.payoneer.checkout.payment.PaymentServiceInteractor;
+import com.payoneer.checkout.ui.screen.shared.ProgressSettings;
+import com.payoneer.checkout.ui.session.PaymentSessionInteractor;
+import com.payoneer.checkout.util.Event;
+import com.payoneer.checkout.util.LiveDataUtil;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import java.net.URL;
+
+@RunWith(RobolectricTestRunner.class)
 public class ProcessPaymentViewModelTest {
 
+    private final CheckoutConfiguration configuration =
+            CheckoutConfiguration.createBuilder(createUrl()).build();
+    private final ProcessPaymentViewModel viewModel =
+            new ProcessPaymentViewModel(ApplicationProvider.getApplicationContext(), new PaymentSessionInteractor(configuration), new PaymentServiceInteractor());
+
+    @Test
+    public void onProcessPaymentResume_setsProgressAndProcessOaymentLiveDataCorrectly() throws InterruptedException {
+        viewModel.onProcessPaymentResume();
+
+        Event event = LiveDataUtil.getOrAwaitValue(viewModel.showProcessPaymentFragment()).getIfNotHandled();
+        ProgressSettings progressSettings = LiveDataUtil.getOrAwaitValue(viewModel.showProcessPaymentProgress()).getContentIfNotHandled();
+
+        assertNotNull(event);
+        assertNotNull(progressSettings);
+        assertTrue(progressSettings.visible);
+    }
+
+    private URL createUrl() {
+        try {
+            return new URL("https://raw.githubusercontent.com/optile/checkout-android/develop/shared-test/lists/listresult.json");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }

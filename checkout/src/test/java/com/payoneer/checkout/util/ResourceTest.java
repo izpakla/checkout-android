0b3a29766ee101a104ccc34c312a71902e608e06
Siebe Brouwer <siebe@siebeprojects.com>
2022-08-17 17:54:40 +0200

    PCX-3845: Add unit tests

---
+++ checkout/src/test/java/com/payoneer/checkout/util/ResourceTest.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2022 Payoneer Germany GmbH
+ * https://www.payoneer.com
+ *
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more information.
+ */
+
+package com.payoneer.checkout.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ResourceTest {
+
+    @Test
+    public void success() {
+        Integer data = new Integer(100);
+        Resource<Integer> res = Resource.success(data);
+        assertEquals(res.getStatus(), Resource.SUCCESS);
+        assertEquals(data, res.getData());
+        assertNull(res.getMessage());
+    }
+
+    @Test
+    public void error() {
+        String message = "Error message";
+        Resource<String> res = Resource.error(message);
+        assertEquals(res.getStatus(), Resource.ERROR);
+        assertEquals(message, res.getMessage());
+        assertNull(res.getData());
+    }
+
+    @Test
+    public void loading() {
+        Resource<String> res = Resource.loading();
+        assertEquals(res.getStatus(), Resource.LOADING);
+        assertNull(res.getMessage());
+        assertNull(res.getData());
+    }
+}

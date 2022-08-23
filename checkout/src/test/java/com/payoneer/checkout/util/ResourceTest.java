16615e3a40a7d99d7cf0b54386be26facd94c12d
Siebe Brouwer <siebe@siebeprojects.com>
2022-08-23 17:07:23 +0200

    PCX-3845: Code cleanup

--- checkout/src/test/java/com/payoneer/checkout/util/ResourceTest.java
+++ checkout/src/test/java/com/payoneer/checkout/util/ResourceTest.java
@@ -8,7 +8,8 @@
 
 package com.payoneer.checkout.util;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 import org.junit.Test;
 
